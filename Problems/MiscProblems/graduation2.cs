4619f5303bf2963664970fb1c758494e70fced86
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-03-22 16:54:17 +0200

    Create graduation2.cs

---
+++ Problems/MiscProblems/graduation2.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graduation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 0;
+            string done = "";
+
+            done = Console.ReadLine();
+            i = Convert.ToInt32(done);
+            Console.WriteLine(i);
+        }
+    }
+}
