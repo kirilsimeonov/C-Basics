7285ed4dec63da54648f8ad8a7b067d8404b18e0
Kiril Simeonov <45626567+kirilsimeonov@users.noreply.github.com>
2019-03-25 13:45:54 +0200

    Create Trekking Equipment.cs

---
+++ Exam score 100%/Trekking Equipment.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6___Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alpinist = int.Parse(Console.ReadLine());
+            int karabineri = int.Parse(Console.ReadLine());
+            int ropes = int.Parse(Console.ReadLine());
+            int pikels = int.Parse(Console.ReadLine());
+
+            double karabineriPrice = 36;
+            double ropesPrice = 3.6;
+            double pikelsPrice = 19.8;
+
+            double karabineriTotal = karabineri * karabineriPrice;
+            double ropesTotal = ropes * ropesPrice;
+            double pikelsTotal = pikels * pikelsPrice;
+            double totalSumOneAlpinist = karabineriTotal + ropesTotal + pikelsTotal;
+            double totalSum = totalSumOneAlpinist * alpinist;
+            Console.WriteLine($"{totalSum * 1.2:f2}");
+
+        }
+    }
+}
